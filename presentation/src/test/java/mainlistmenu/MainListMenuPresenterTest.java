74ef170c8ab86d3409e94ee369a02bdefb6ded16
GreenLion Soft <support@greenlionsoft.com>
2015-11-01 23:47:25 +0100

    fixed unit test

--- presentation/src/test/java/mainlistmenu/MainListMenuPresenterTest.java
+++ presentation/src/test/java/mainlistmenu/MainListMenuPresenterTest.java
@@ -19,6 +19,8 @@ public class MainListMenuPresenterTest {
 
         mockMainListMenuView = mock(IMainListMenuView.class);
 
+        presenter = new MainListMenuPresenter(mockMainListMenuView);
+
     }
 
     @Test
