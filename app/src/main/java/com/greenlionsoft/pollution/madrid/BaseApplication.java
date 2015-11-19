4dd18fc8276fb5eb048fbc766d2eca36ea3a3a39
GreenLionSoft <support@greenlionsoft.com>
2015-11-19 05:54:26 +0100

    configured Answers and Crashlytics with Fabric

--- app/src/main/java/com/greenlionsoft/pollution/madrid/BaseApplication.java
+++ app/src/main/java/com/greenlionsoft/pollution/madrid/BaseApplication.java
@@ -2,13 +2,17 @@ package com.greenlionsoft.pollution.madrid;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.greenlionsoft.pollution.madrid.dependencies.AppInjector;
+
+import io.fabric.sdk.android.Fabric;
 
 public class BaseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         AppInjector.getInstance().setApplicationContext(getApplicationContext());
 
