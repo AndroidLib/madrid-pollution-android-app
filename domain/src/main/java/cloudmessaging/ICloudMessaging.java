81ec95627eef50245fbec75b99cfe132118996bd
GreenLionSoft <support@greenlionsoft.com>
2015-12-14 07:04:00 +0100

    added push message bundle keys as constants

--- domain/src/main/java/cloudmessaging/ICloudMessaging.java
+++ domain/src/main/java/cloudmessaging/ICloudMessaging.java
@@ -2,5 +2,12 @@ package cloudmessaging;
 
 public interface ICloudMessaging {
 
+
+    String TRAFFIC_RESTRICTION_ALERT = "traffic_restriction_alert";
+    String TYPE = "type";
+    String DATE = "date";
+    String LEVEL = "level";
+    String DEBUG_ONLY = "debug_only";
+
     void register();
 }
